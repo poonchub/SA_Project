f75abc0f0e9d856f037515f293ae3984a919d8d5
poonchub <141898614+poonchub@users.noreply.github.com>
2024-09-08 23:21:33 +0700

    8/9/67

--- IT Shop/frontend/src/pages/Home/Home.tsx
+++ IT Shop/frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Header from "../../components/Header/Header";
 import { AppContext } from "../../App";
 import "./Home.css"
