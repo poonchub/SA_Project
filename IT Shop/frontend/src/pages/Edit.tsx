de1298d500092613d18c0615c2008a409c5ec168
Rungnaphaaa <earnrungnapha25@gmail.com>
2024-09-10 15:23:44 +0700

    profile

--- IT Shop/frontend/src/pages/Edit.tsx
+++ IT Shop/frontend/src/pages/Edit.tsx
@@ -1,11 +1,18 @@
 import Header from "../components/Header/Header";
 import EditProfile from "../components/EditProfile/EditProfile.tsx";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 function Edit(){
+    
+    const {logoutPopup} = useContext(AppContext)
 
     return (
         <>
-            <Header page={"edit"}/>
+            
+            
+            {logoutPopup}
+            <Header page={"profile"}/>
             <EditProfile/>
         </>
     )
--- IT Shop/frontend/src/pages/Profile.tsx
+++ IT Shop/frontend/src/pages/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import Header from "../components/Header/Header";
+import { AppContext } from "../App";
 import ShowProfile from "../components/ShowProfile/ShowProfile";
 
 function Profile(){
+    const {logoutPopup} = useContext(AppContext)
 
     return (
-        <>
+        <>  
+            {logoutPopup}
             <Header page={"profile"}/>
             <ShowProfile/>
         </>
     )
 }
 
-export default Profile
+export default Profile;
