8e3b9830ced7aab25e34bcec8f648b4116f7d44b
Namzxn <141898614+poonchub@users.noreply.github.com>
2024-09-13 16:07:43 +0700

    Update Profile.tsx

--- IT Shop/frontend/src/pages/Profile.tsx
+++ IT Shop/frontend/src/pages/Profile.tsx
@@ -2,19 +2,12 @@ import { useContext } from "react";
 import Header from "../components/Header/Header";
 import { AppContext } from "../App";
 import ShowProfile from "../components/ShowProfile/ShowProfile";
-import { AppContext } from "../App";
-import { useContext } from "react";
 
 function Profile(){
-    const {logoutPopup} = useContext(AppContext)
 
     const {logoutPopup} = useContext(AppContext)
     return (
-<<<<<<< HEAD
-        <>
-=======
         <>  
->>>>>>> main
             {logoutPopup}
             <Header page={"profile"}/>
             <ShowProfile/>
